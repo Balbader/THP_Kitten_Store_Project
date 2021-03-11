5ec6240685fbded09006c401f850a214e2109d64
Yann Rannou <yann.rannou777@gmail.com>
2021-03-11 15:05:55 +0100

    User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
   root 'shops#index'
   resources :shops, only:[:index, :show]
   resources :items
+  resources :users
   resources :order_items
   resource :cards, only:[:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-prices = [10.99, 11.99, 12.99, 13.99, 14.99, 15.99, 16.99, 17.99, 18.99, 19.99]
+prices = [123.99, 567.99, 1112.99, 138.99, 154.99, 105.99, 163.99, 197.99, 1890.99, 3999.99]
 
  10.times do
-  Item.create!(title: Faker::Book.title, description: Faker::Lorem.sentence(5), price: prices.sample, image_url: Faker::LoremFlickr.image)
+  product.create(title: Faker::Creature::Cat.name, price: prices.sample)
  end
 
 #User.create!(email: "admin@admin.com", first_name: "Admin", last_name: "The Boss", password: "admin01", is_admin: true)
@@ -17,12 +17,12 @@ prices = [10.99, 11.99, 12.99, 13.99, 14.99, 15.99, 16.99, 17.99, 18.99, 19.99]
 #   User.create!(email: "user#{i+2}@user.com", password: "tests#{i+2}", first_name: "José #{i+2}", last_name: "Josiane #{i+2}")
 # end
 
-# 1.times do |i|
-#   CartItem.create!(cart_id: 1, item_id: rand(1..20))
-# end
+ 1.times do |i|
+   CartItem.create!(cart_id: 1, item_id: rand(1..20))
+ end
 
 
-# 1.times do |i|
-#   Order.create!(user_id: 1, cart_id: i+1, total: 100)
-# end
+ 1.times do |i|
+  Order.create!(user_id: 1, cart_id: i+1, total: 100)
+end
 # puts "order cree"
